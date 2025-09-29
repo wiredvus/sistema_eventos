6bcd7e8e7f41e46380a6b197504ec75686a60edb
Vinicius <vusantonio@proton.me>
2025-09-29 17:27:50 -0300

    Mover Arquivo

--- index.php
+++ index.php
@@ -1,11 +1,63 @@
+<?php
+// Armazenar as mensagens no sistema
+$ok = '';
+$erro = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $titulo = trim($_POST['titulo'] ?? '');
+    $data = trim($_POST['data'] ?? '');
+    $hora = trim($_POST['hora'] ?? '');
+    $local = trim($_POST['local'] ?? '');
+    $descricao = trim($_POST['descricao'] ?? '');
+
+    // Validação se algum campo obrigatório estiver vázio Exiba mensagem
+
+    if (!$titulo || !$data || !$hora || !$local || !$descricao) {
+        $erro = "Preencha todos os campos !";
+    }
+
+    // Upload Imagem
+    $caminhoImagem = '';
+    if (empty($erro) && !empty($_FILES['imagem']['name']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
+
+        // Tamanho maximo de uma imagem 2mb
+        $tamMax = 2 * 1024 * 124;
+        // Se o arquivo for > Que o limite
+        if ($_FILES['imagem']['size'] > $tamMax) {
+            $erro = "Imagem ta grande em !! (Máx. 2mb)";
+        } else {
+            // Criamos uma pasta para armazenar as imagens caso não exista
+            // Migramos para esta pasta
+
+            $nomeOrignal = basename((string)$_FILES['imagem']['name']);
+            $nomeOrignal = preg_replace('/[^A-Za-z0-9_.-', '_', $nomeOrignal);
+            $nomeFinal = time() . '_' . $nomeOriginal;
+
+            $destino = __DIR__ . '/uploads/' . $nomeFinal;
+
+            // Mover arquivo para a pasta
+            if (move_uploaded_file($_FILES['imagem']['tmp_name'], $destino)) {
+                $caminhoImagem = 'uploads/' . $nomeFinal;
+            } else {
+                $erro = "Falha ao salvar a imagem";
+            }
+        }
+        if ($empty($erro)) {
+            echo "DEU TUDO CERTOOOO";
+        }
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Sistema</title>
 </head>
+
 <body>
     <div class="bg">
     </div>
@@ -17,7 +69,7 @@
         <section class="grid">
             <div class="card">
                 <h2 class="card_titulo">Cadastrar Atividade</h2>
-                <form class="form" ="post" enctype="multipart/form-data">
+                <form class="form" method="post" enctype="multipart/form-data">
                     <label for="campo" class="campo">
                         <span>Título</span>
                         <input type="text" name="titulo" id="titulo" require>
@@ -51,7 +103,7 @@
     </div>
     <footer class="rodape">
         <div class="container">
-            <p>Feito em PHP 🩷</p>
+            <p>Feito em PHP pelo <a href="https://github.com/wiredvus">Vus</a> 🩷</p>
         </div>
     </footer>
 </body>
